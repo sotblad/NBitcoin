20f7fe20d34cbc1d0a955ca52e15c18ee1303fa2
Sotiris Blad <sotblad@monetaryunit.org>
2020-06-17 02:30:31 +0300

    Update sample_tests.cs

--- NBitcoin.Tests/sample_tests.cs
+++ NBitcoin.Tests/sample_tests.cs
@@ -15,10 +15,10 @@ namespace NBitcoin.Tests
 			using (var nodeBuilder = NodeBuilderEx.Create())
 			{
 				var rpc = nodeBuilder.CreateNode().CreateRPCClient();
-				Assert.Equal(666, rpc);
+				Assert.Equal('test', rpc);
 				
 				nodeBuilder.StartAll();
-				Assert.Equal(667, nodeBuilder);
+				Assert.Equal('test', nodeBuilder);
 				rpc.Generate(102);
 
 				// Build the keys and addresses
